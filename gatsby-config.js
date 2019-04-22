19744f69ebbdb3cabf88060358e5d399adc3bc22
Tatiana Scott <hello@tatianascott.com>
2019-04-22 19:44:28 -0400

    gitignore file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,20 @@
+require('dotenv').config()
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-material-ui`,
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-138905713-1",
+        // your google analytics tracking id
+        trackingId: GOOGLE_ANALYTICS_ID,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
       },
-    }
+    },
   ],
 };
