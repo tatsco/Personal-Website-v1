d56e44ad2c5c1ad8db9e61b404575073bed4a59c
Tatiana Scott <hello@tatianascott.com>
2019-04-22 19:47:14 -0400

    Removing ignored files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/tatianascott 1/AllThingsCode/PersonalProjects/Personal-Website/node_modules/gatsby-plugin-material-ui/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
